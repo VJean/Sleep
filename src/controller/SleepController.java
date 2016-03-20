6b5857c472b3df901cb8745e1a689efc37697d8f
Vintache Jean <jean.vintache@etu.utc.fr>
2016-03-20 16:27:30 +0100

    better spinners for the hours and minutes fields

--- src/controller/SleepController.java
+++ src/controller/SleepController.java
@@ -1,21 +1,25 @@
 package controller;
 
+import javafx.beans.NamedArg;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class SleepController {
     @FXML
     private DatePicker beginDatePicker;
     @FXML
-    private TextField beginHourField;
+    private Spinner beginHourField;
     @FXML
-    private TextField beginMinuteField;
+    private Spinner beginMinuteField;
     @FXML
     private DatePicker endDatePicker;
     @FXML
-    private TextField endHourField;
+    private Spinner endHourField;
     @FXML
-    private TextField endMinuteField;
+    private Spinner endMinuteField;
     @FXML
     private CheckBox aloneCheckBox;
     @FXML
@@ -27,7 +31,49 @@ public class SleepController {
     }
 
     public void initialize() {
+        LocalDate today = LocalDate.now();
+        LocalDate yesterday = today.minusDays(1);
+
+        beginDatePicker.setValue(yesterday);
+        beginHourField.setValueFactory(new LoopingIntegerSpinnerValueFactory(0, 23, 23));
+        beginMinuteField.setValueFactory(new LoopingIntegerSpinnerValueFactory(0, 59, 0));
+
+        endDatePicker.setValue(today);
+        endHourField.setValueFactory(new LoopingIntegerSpinnerValueFactory(0, 23, 8));
+        endMinuteField.setValueFactory(new LoopingIntegerSpinnerValueFactory(0, 59, 30));
+
 
     }
 
+    /**
+     * An IntegerSpinnerValueFactory that loops on its value list :
+     * - pressing the upArrow of the control when its value is already at max sets it to min;
+     * - pressing the downArrow of the control when its value is already at min sets it to max
+     */
+    private class LoopingIntegerSpinnerValueFactory extends SpinnerValueFactory.IntegerSpinnerValueFactory {
+
+        public LoopingIntegerSpinnerValueFactory(@NamedArg("min") int min, @NamedArg("max") int max) {
+            super(min, max);
+        }
+
+        LoopingIntegerSpinnerValueFactory(@NamedArg("min") int min, @NamedArg("max") int max, @NamedArg("initialValue") int initialValue) {
+            super(min, max, initialValue);
+        }
+
+        @Override
+        public void decrement(int steps) {
+            if (getValue() - steps < getMin())
+                setValue(getMax() - steps - getValue() - getMin() + 1);
+            else
+                super.decrement(steps);
+        }
+
+        @Override
+        public void increment(int steps) {
+            if (getValue() + steps > getMax())
+                setValue(getMin() + steps - getMax() - getValue() - 1);
+            else
+                super.increment(steps);
+        }
+    }
 }
