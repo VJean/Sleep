d2a5623538650525493dffdb0b7caedcb8629b1c
Vintache Jean <jean.vintache@etu.utc.fr>
2016-04-04 08:42:22 +0200

    SleepItem comparison methods

--- src/model/SleepItem.java
+++ src/model/SleepItem.java
@@ -9,8 +9,9 @@ import java.time.LocalDateTime;
 
 /**
  * Created by JeanV on 20/03/2016.
+ * Note: this class has a natural ordering that is inconsistent with equals.
  */
-public class SleepItem {
+public class SleepItem implements Comparable<SleepItem> {
     private int id;
     private LocalDateTime begin;
     private LocalDateTime end;
@@ -88,5 +89,34 @@ public class SleepItem {
     public String toString() {
         // just the date
         return "(id" + String.valueOf(this.id) + ") " + this.end.toLocalDate().toString();
+    }
+
+    public boolean isBefore(SleepItem other) {
+        return this.end.isBefore(other.begin);
+    }
+
+    public boolean isAfter(SleepItem other) {
+        return this.begin.isAfter(other.end);
+    }
+
+    public boolean overlaps(SleepItem other) {
+        return (this.begin.isAfter(other.begin) && this.begin.isBefore(other.end))
+                || (this.end.isAfter(other.begin) && this.end.isBefore(other.end));
+    }
+
+    /**
+     * Compares this object with the specified object for order.  Returns a
+     * negative integer, zero, or a positive integer as this object is less
+     * than, equal to, or greater than the specified object.
+     * @param o the object to be compared.
+     * @return a negative integer, zero, or a positive integer as this object
+     * is less than, equal to, or greater than the specified object.
+     * @throws NullPointerException if the specified object is null
+     * @throws ClassCastException   if the specified object's type prevents it
+     *                              from being compared to this object.
+     */
+    @Override
+    public int compareTo(SleepItem o) {
+        return this.begin.compareTo(o.begin);
     }
 }
